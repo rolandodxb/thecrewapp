562821e19dd610b957663dd1bd4dc116135274c7
tito24dxb <titodorao24@gmail.com>
2025-11-26 18:21:15 -0600

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { enableOfflineSupport, registerServiceWorker } from './utils/enableOfflineSupport';
-import { initializeTelemetry } from './lib/telemetry';
 
 if (!localStorage.getItem('appInitialized')) {
   localStorage.clear();
   localStorage.setItem('appInitialized', 'true');
 }
 
-initializeTelemetry();
+// Telemetry disabled - causing dev server crash
+// initializeTelemetry();
 enableOfflineSupport();
 registerServiceWorker();
 
